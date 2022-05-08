03a3d0328608864af8f81ff9cf7ebbe9b23865b0
Madhan-Offi <102455806+Madhan-Offi@users.noreply.github.com>
2022-05-08 14:53:56 +0530

    Bot

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -105,8 +105,8 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    p_str = '▰' * cFull
-    p_str += '▱' * (12 - cFull)
+    p_str = '●' * cFull
+    p_str += '○' * (12 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
@@ -125,7 +125,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"<b>Name:</b> <code>{download.name().replace('<', '')}</code>"
+            msg += f"<b>Filename:</b> <code>{download.name().replace('<', '')}</code>"
             msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
